5c4c3b3e5a67202cb8528a63447d036d6f42bde4
Anantha Boudmanabhan <ananth1995.july22@gmail.com>
2021-08-09 15:29:45 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import { Container, Switch, withStyles } from '@material-ui/core';
+import { grey } from '@material-ui/core/colors';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Definitions from './components/definitions/Definitions';
+import Header from './components/header/Header';
 
 function App() {
+
+  const [category, setCategory] = useState("en");
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [lightMode, setLightMode] = useState(false);
+
+  const DarkMode = withStyles({
+    switchBase: {
+      color: grey[300],
+      '&$checked': {
+        color: grey[500],
+      },
+      '&$checked + $track': {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);
+      setMeanings(data.data);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  };
+
+
+  useEffect(() => {
+    dictionaryApi();
+  }, [word, category]);
+
+  console.log(meanings);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: "100vh", backgroundColor: lightMode ? "#fff" : "#282c34", color: lightMode ? "black" : "#fff", transition: "all 0.5s linear" }}>
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh", justifyContent: "space-evenly" }}
+      >
+        <div style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}>
+          <span>{lightMode ? "Dark" : "Light"} Mode</span>
+          <DarkMode checked={lightMode} onChange={() => setLightMode((prevState) => !prevState)} />
+        </div>
+        <Header
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setWord={setWord}
+          lightMode={lightMode}
+        />
+        {meanings && <Definitions
+          word={word}
+          meanings={meanings}
+          category={category}
+          lightMode={lightMode}
+        />}
+      </Container>
     </div>
   );
 }
